df338e3f5dc0757806359333f6bf3fd5e413589a
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-05 11:58:40 +0100

    wrong http status code. Fixed 201 -> 204

--- src/module/common/application/http/status.py
+++ src/module/common/application/http/status.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/status", tags=["status"])
 
 
-@router.get("/health", status_code=201)
+@router.get("/health", status_code=204)
 async def health():
     pass
