c866baeaac7deded97ed10845ed1dcd21f80d7ff
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-05 21:45:19 +0100

    comment not required feature

--- src/module/placemanager/application/http/category.py
+++ src/module/placemanager/application/http/category.py
@@ -1,28 +1,24 @@
-from typing import Annotated, Optional
-
 from fastapi import APIRouter
-from fastapi.params import Query
 from pydantic import BaseModel, ConfigDict, Field
-
-from src.module.common.application.http import PaginatedResponse, QueryParams
 
 router = APIRouter(prefix="/categories", tags=["category"])
 
 
-class CategoryResponse(BaseModel):
-    model_config = ConfigDict(frozen=True)
-    id: str
-    name: str
-
-
-class CategoryPagination(QueryParams):
-    next_token: Optional[str] = None
-
-
-@router.get("/")
-async def get_all_categories(
-    query: Annotated[CategoryPagination, Query()]
-) -> PaginatedResponse[CategoryResponse, str]: ...
+# TODO: Clean up on final releases
+# class CategoryResponse(BaseModel):
+#     model_config = ConfigDict(frozen=True)
+#     id: str
+#     name: str
+#
+#
+# class CategoryPagination(QueryParams):
+#     next_token: Optional[str] = None
+#
+#
+# @router.get("/")
+# async def get_all_categories(
+#     query: Annotated[CategoryPagination, Query()]
+# ) -> PaginatedResponse[CategoryResponse, str]: ...
 
 
 class CreateCategory(BaseModel):
