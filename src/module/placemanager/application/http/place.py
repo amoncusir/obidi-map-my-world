c0930f740ae7cc01fff7328f0d1afbaf135303c0
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-05 14:04:37 +0100

    add method to create a review for a place

--- src/module/placemanager/application/http/place.py
+++ src/module/placemanager/application/http/place.py
@@ -6,9 +6,9 @@ router = APIRouter(prefix="/places", tags=["place"])
 
 class CreatePlace(BaseModel):
     model_config = ConfigDict(frozen=True)
-    latitude: float = Field(alias="lat", description="Latitude", allow_inf_nan=False, ge=-90.0, le=90.0)
-    longitude: float = Field(alias="lon", description="Longitude", allow_inf_nan=False, ge=-180.0, le=180.0)
-    category: str = Field(description="Category")
+    latitude: float = Field(alias="lat", allow_inf_nan=False, ge=-90.0, le=90.0)
+    longitude: float = Field(alias="lon", allow_inf_nan=False, ge=-180.0, le=180.0)
+    category: str = Field(description="Category ID")
 
 
 class CreatePlaceResponse(BaseModel):
@@ -18,3 +18,12 @@ class CreatePlaceResponse(BaseModel):
 
 @router.post("/")
 async def create_place(place: CreatePlace) -> CreatePlaceResponse: ...
+
+
+class CreateReview(BaseModel):
+    model_config = ConfigDict(frozen=True)
+    rate: int = Field(description="Rate of review", ge=0, le=5)
+
+
+@router.post("/{place_id}/review", status_code=201)
+async def review_place(place_id: str, review: CreateReview): ...
