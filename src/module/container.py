59ab96456ae8b364afaaf8c4034192df43f15f0e
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-06 00:03:15 +0100

    defined the dependency graph

--- src/module/container.py
+++ src/module/container.py
@@ -1,3 +1,6 @@
+from itertools import chain
+from typing import Any, List
+
 from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
 
@@ -5,6 +8,10 @@ from src.module.common.container import CommonContainer
 from src.module.geoquerier.container import GeoQuerierContainer
 from src.module.manager.container import ManagerContainer
 from src.module.recommendation.container import RecommendationContainer
+
+
+def plain_list(*lists: List[Any]) -> List[Any]:
+    return list(chain.from_iterable(lists))
 
 
 class ModuleContainer(DeclarativeContainer):
@@ -17,3 +24,5 @@ class ModuleContainer(DeclarativeContainer):
     manager_container = providers.Container(ManagerContainer, config=config.manager)
 
     recommendation_container = providers.Container(RecommendationContainer, config=config.recommendation)
+
+    commands = providers.Factory(plain_list, manager_container.list_commands)
--- src/module/manager/container.py
+++ src/module/manager/container.py
@@ -1,6 +1,64 @@
+from types import NoneType
+
 from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
+from pymongo.database import Database
+
+from src.module.manager.domain.category import CategoryRepository
+from src.module.manager.domain.command.create_category import (
+    CreateCategoryCommandHandler,
+)
+from src.module.manager.domain.command.create_place import CreatePlaceCommandHandler
+from src.module.manager.domain.place import PlaceRepository
+from src.module.manager.infrastructure.mongodb.category_repository import (
+    MongoCategoryRepository,
+)
+from src.module.manager.infrastructure.mongodb.place_repository import (
+    MongoPlaceRepository,
+)
+
+
+class Repository(DeclarativeContainer):
+    database = providers.Dependency(Database)
+
+    mongo_place_repository = providers.Singleton(MongoPlaceRepository, database=database)
+
+    place_repository = providers.Dependency(instance_of=PlaceRepository, default=mongo_place_repository)
+
+    mongo_category_repository = providers.Singleton(MongoCategoryRepository, database=database)
+
+    category_repository = providers.Dependency(instance_of=CategoryRepository, default=mongo_category_repository)
+
+
+class Command(DeclarativeContainer):
+    repository = providers.DependenciesContainer()
+
+    create_category = providers.Singleton(
+        CreateCategoryCommandHandler, category_repository=repository.category_repository
+    )
+
+    create_place = providers.Singleton(
+        CreatePlaceCommandHandler,
+        category_repository=repository.category_repository,
+        place_repository=repository.place_repository,
+    )
 
 
 class ManagerContainer(DeclarativeContainer):
     config = providers.Configuration()
+    database = providers.Dependency(Database)
+
+    repository = providers.Container(
+        Repository,
+        database=database,
+    )
+
+    command = providers.Container(
+        Command,
+        repository=repository,
+    )
+
+    list_commands = providers.List(
+        command.create_category,
+        command.create_place,
+    )
