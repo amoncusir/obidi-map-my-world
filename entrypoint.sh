616e341bc47858f5bb63b6c40a5f9ca9003799e4
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-06 14:09:43 +0100

    create the Dockerfile for the service

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -x
+
+# Function to run the API
+run_api() {
+  echo "Starting API with arguments: $@"
+  uvicorn "$@" 'src.app.api_instance:api'
+}
+
+# Check if at least one argument is provided
+if [ -z "$1" ]; then
+  echo "You must provide the <mode> argument"
+  exit 1
+fi
+
+# Capture the first argument as the mode to run
+mode=$1
+shift # Remove the first argument to pass the remaining arguments
+
+# Execute the corresponding function based on the first argument
+case "$mode" in
+  api)
+    run_api "$@"
+    ;;
+  *)
+    echo "Invalid option. Use 'api',"
+    exit 1
+    ;;
+esac
