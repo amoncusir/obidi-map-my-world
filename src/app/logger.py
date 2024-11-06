cdd38497b096b30f7b297e7a6d21c49e76c63c2e
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-06 17:59:40 +0100

    set a custom logger

--- src/app/logger.py
+++ src/app/logger.py
@@ -1,7 +1,33 @@
 import logging
 import os
+import sys
+from logging import DEBUG, Formatter, Handler, getLogger
 from typing import Final
 
 LOG_LEVEL: Final[int] = os.environ.get("LOG_LEVEL", logging.DEBUG)
 
-logging.basicConfig(level=LOG_LEVEL)
+
+def build_formatter() -> Formatter:
+    formatter = Formatter("%(levelname)s:\t%(name)s --- %(message)s")
+    return formatter
+
+
+def build_handler() -> Handler:
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = build_formatter()
+    handler.setFormatter(formatter)
+
+    return handler
+
+
+def configure_logger():
+    handler = build_handler()
+
+    project_logger = getLogger("src")
+    project_logger.setLevel(DEBUG)
+    project_logger.addHandler(handler)
+    project_logger.propagate = False
+
+
+configure_logger()
