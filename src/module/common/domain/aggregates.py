8eb4f807a79dd6a99b61a9001bc13984af2c532f
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-06 21:57:41 +0100

    add events manage

--- src/module/common/domain/aggregates.py
+++ src/module/common/domain/aggregates.py
@@ -1,5 +1,19 @@
+from typing import List
+
+from pydantic import Field
+
 from src.module.common.domain.entities import DomainEntity
+from src.module.common.domain.events import DomainEvent
 
 
 class AggregateRoot[ID](DomainEntity[ID]):
-    pass
+
+    __events_queue: List[DomainEvent] = Field(default_factory=list, exclude=True)
+
+    def _add_event(self, event: DomainEvent):
+        self.__events_queue.append(event)
+
+    def pop_events(self) -> List[DomainEvent]:
+        flushed_events = self.__events_queue.copy()
+        self.__events_queue = []
+        return flushed_events
