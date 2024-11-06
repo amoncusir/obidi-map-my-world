777fd03704b97c234f83fbd6ca104ec981dbc723
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-06 22:52:06 +0100

    create event domain bus

---
+++ src/module/common/application/domain_event_bus.py
@@ -0,0 +1,46 @@
+import asyncio
+from asyncio import TaskGroup
+from functools import singledispatchmethod
+from typing import Dict, List, Type
+
+from src.module.common.domain.events import DomainEvent, DomainEventSubscriber
+
+
+class DomainEventBus:
+
+    subscribers: Dict[Type[DomainEvent], List[DomainEventSubscriber]]
+
+    def __init__(self, subscribers: List[DomainEventSubscriber]):
+        self.subscribers = {}
+
+        for subscriber in subscribers:
+            event_type = subscriber.event_type()
+
+            if event_type not in self.subscribers:
+                self.subscribers[event_type] = []
+
+            self.subscribers[event_type].append(subscriber)
+
+    @singledispatchmethod
+    def find_subscribers(self, event_type: Type[DomainEvent]) -> List[DomainEventSubscriber]:
+        subscribers = self.subscribers.get(event_type, None)
+
+        if subscribers is None:
+            return []
+
+        return subscribers
+
+    @find_subscribers.register
+    def _(self, event: DomainEvent) -> List[DomainEventSubscriber]:
+        return self.find_subscribers(type(event))
+
+    async def async_trigger(self, event: DomainEvent):
+        subscribers: List[DomainEventSubscriber] = self.find_subscribers(event)
+
+        async with TaskGroup() as task_group:
+            for subscriber in subscribers:
+                task_group.create_task(subscriber(event))
+
+    def trigger(self, event: DomainEvent):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.async_trigger(event))
