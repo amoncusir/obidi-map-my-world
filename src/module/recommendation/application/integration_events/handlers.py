894aa485d057bbf04e00f174adb006b3b4dac31a
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 22:50:01 +0100

    first implementation to test

--- src/module/recommendation/application/integration_events/handlers.py
+++ src/module/recommendation/application/integration_events/handlers.py
@@ -1,6 +1,23 @@
+from src.module.common.application.event.integration import IntegrationEventSubscriber
 from src.module.manager.application.integration_events.events import (
     ReviewAddedApplicationEvent,
 )
 
 
-def manager_place_review_added_handler(event: ReviewAddedApplicationEvent): ...
+class ReviewAddedIntegrationEventSubscriber(IntegrationEventSubscriber[ReviewAddedApplicationEvent]):
+    """
+    Subscription to ReviewAddedApplicationEvent.
+    Pass through the recommendation layer to decide if it changes
+    """
+
+    async def handle_event(self, event: ReviewAddedApplicationEvent):
+        print(event)
+        print("AMZNG!")
+
+    @classmethod
+    def name(cls) -> str:
+        return "place.updated.review_added.to.recommendation"
+
+    @classmethod
+    def routing_key(cls) -> str:
+        return "place.updated.review_added"
