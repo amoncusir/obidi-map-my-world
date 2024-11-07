d9d38826ae632e67ca12930d47f28d4b5d779f8f
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 17:39:22 +0100

    implement the bus

--- src/module/common/infrastructure/celery/integration_events_bus.py
+++ src/module/common/infrastructure/celery/integration_events_bus.py
@@ -1,7 +1,22 @@
+from dataclasses import dataclass
+
+from celery import Celery
+from celery.result import AsyncResult
+
+from src.app.worker_instance import celery
 from src.module.common.application.integration_events import IntegrationEvent
-from src.module.common.application.integration_events_bus import IntegrationEventsBus
+from src.module.common.application.integration_events_bus import (
+    IntegrationEventsBus,
+    IntegrationEventTask,
+)
 
 
+@dataclass
 class CeleryIntegrationEventsBus(IntegrationEventsBus):
 
-    async def async_trigger(self, event: IntegrationEvent): ...
+    celery: Celery
+
+    async def async_trigger(self, event: IntegrationEvent) -> IntegrationEventTask:
+        result: AsyncResult = celery.send_task(event.name(), args=[event.model_dump()])
+
+        return IntegrationEventTask(id=result.task_id)
