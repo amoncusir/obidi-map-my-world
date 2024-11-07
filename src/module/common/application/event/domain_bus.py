d21c37cdd74e42afcee6f114b1e946379b1e245a
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 21:16:58 +0100

    removed non async method, to prevent inconsistent states

--- src/module/common/application/event/domain_bus.py
+++ src/module/common/application/event/domain_bus.py
@@ -12,21 +12,10 @@ class DomainEventBus:
     @abstractmethod
     async def async_trigger(self, event: DomainEvent): ...
 
-    def trigger(self, event: DomainEvent):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.async_trigger(event))
-
     async def async_trigger_list(self, events: List[DomainEvent]):
         async with TaskGroup() as task_group:
             for event in events:
                 task_group.create_task(self.async_trigger(event))
 
-    def trigger_list(self, events: List[DomainEvent]):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.async_trigger_list(events))
-
     async def async_trigger_aggregate(self, aggregate: AggregateRoot):
         await self.async_trigger_list(aggregate.pop_events())
-
-    def trigger_aggregate(self, aggregate: AggregateRoot):
-        self.trigger_list(aggregate.pop_events())
