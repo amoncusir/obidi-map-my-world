e09bb9ba132fff98c576502efeb4f63c262f75b5
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 21:16:04 +0100

    add a non async method to shot-and-forget event

--- src/module/common/application/event/integration_bus.py
+++ src/module/common/application/event/integration_bus.py
@@ -1,3 +1,4 @@
+import asyncio
 from abc import abstractmethod
 from dataclasses import dataclass
 
@@ -13,3 +14,7 @@ class IntegrationEventsBus:
 
     @abstractmethod
     async def async_publish(self, event: IntegrationEvent) -> IntegrationEventTask: ...
+
+    def publish(self, event: IntegrationEvent):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.async_publish(event))
