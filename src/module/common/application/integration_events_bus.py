2528d461ca3f2511d5096e7c32ce99d7b13e33fe
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 17:39:45 +0100

    return a task with an ID

--- src/module/common/application/integration_events_bus.py
+++ src/module/common/application/integration_events_bus.py
@@ -1,9 +1,15 @@
 from abc import abstractmethod
+from dataclasses import dataclass
 
 from src.module.common.application.integration_events import IntegrationEvent
+
+
+@dataclass(frozen=True)
+class IntegrationEventTask:
+    id: str
 
 
 class IntegrationEventsBus:
 
     @abstractmethod
-    async def async_trigger(self, event: IntegrationEvent): ...
+    async def async_trigger(self, event: IntegrationEvent) -> IntegrationEventTask: ...
