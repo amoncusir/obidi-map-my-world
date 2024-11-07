939ed40fdb483b821365c55af0a3444e5bcdeada
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 21:18:45 +0100

    added more generic event type as base

--- src/module/manager/application/integration_events/events.py
+++ src/module/manager/application/integration_events/events.py
@@ -1,21 +1,29 @@
 from src.module.common.application.event.integration import IntegrationEvent
 from src.module.manager.domain.place.projections import (
-    NewPlaceProjection,
     NewReviewedPlaceProjection,
+    PlaceProjection,
 )
 
 
 class CreatedPlaceApplicationEvent(IntegrationEvent):
-    place_projection: NewPlaceProjection
+    place_projection: PlaceProjection
 
     @classmethod
     def name(cls) -> str:
         return "manager.place.created"
 
 
-class ReviewAddedApplicationEvent(IntegrationEvent):
+class UpdatedPlaceApplicationEvent(IntegrationEvent):
+    place_projection: PlaceProjection
+
+    @classmethod
+    def name(cls) -> str:
+        return "manager.place.updated"
+
+
+class ReviewAddedApplicationEvent(UpdatedPlaceApplicationEvent):
     new_review_place: NewReviewedPlaceProjection
 
     @classmethod
     def name(cls) -> str:
-        return "manager.place.review_added"
+        return "manager.place.updated.review_added"
