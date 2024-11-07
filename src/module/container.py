cffaac81e5ae7b82820a20f66f8d605b845e65f1
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-07 17:39:01 +0100

    added integration_event_bus provider

--- src/module/container.py
+++ src/module/container.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from celery import Celery
 from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
 from pymongo.asynchronous.database import AsyncDatabase
@@ -7,6 +8,9 @@ from pymongo.asynchronous.database import AsyncDatabase
 from src.module.common.application.command_bus import CommandBus
 from src.module.common.application.domain_event_bus import DomainEventBus
 from src.module.common.container import CommonContainer
+from src.module.common.infrastructure.celery.integration_events_bus import (
+    CeleryIntegrationEventsBus,
+)
 from src.module.geoquerier.container import GeoQuerierContainer
 from src.module.manager.container import ManagerContainer
 from src.module.providers import CommandHandlerProvider, DomainEventSubscriberProvider
@@ -19,16 +23,21 @@ def list_providers(container: DeclarativeContainer, provider_type: Any):
 
 class ModuleContainer(DeclarativeContainer):
     config = providers.Configuration()
+
     database = providers.Dependency(AsyncDatabase)
+    celery = providers.Dependency(Celery)
+
     __self__ = providers.Self()
 
     domain_event_bus = providers.Singleton(
-        DomainEventBus, subscribers=providers.Singleton(list_providers, __self__, DomainEventSubscriberProvider)
+        DomainEventBus, subscribers=providers.Factory(list_providers, __self__, DomainEventSubscriberProvider)
     )
 
     command_bus = providers.Singleton(
-        CommandBus, handlers=providers.Singleton(list_providers, __self__, CommandHandlerProvider)
+        CommandBus, handlers=providers.Factory(list_providers, __self__, CommandHandlerProvider)
     )
+
+    integration_event_bus = providers.Singleton(CeleryIntegrationEventsBus, celery=celery)
 
     common_container = providers.Container(
         CommonContainer,
