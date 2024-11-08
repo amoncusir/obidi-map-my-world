cd028d4ef204703ec74e567410f553fe674d2f57
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 13:24:50 +0100

    add domain rules for recommendation

---
+++ src/module/recommendation/domain/recommendation/rules.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+from src.module.common.domain.rules import Rule, RuleError
+from src.module.recommendation.domain.recommendation.recommendation import PlaceView
+
+
+class DifferentIDPlaceWhenUpdateRuleError(RuleError):
+    pass
+
+
+@dataclass(frozen=True)
+class UpdatedPlaceMustBeSameID(Rule):
+    current_place: PlaceView
+    new_place: PlaceView
+
+    def __call__(self):
+        if self.current_place is not None and self.current_place.id != self.new_place.id:
+            raise DifferentIDPlaceWhenUpdateRuleError(
+                f"current_id: {self.current_place.id} new_id: {self.new_place.id}"
+            )
+
+
+class OlderPlaceWhenUpdateRuleError(RuleError):
+    pass
+
+
+@dataclass(frozen=True)
+class UpdatedPlaceMustBeNewer(Rule):
+    current_place: PlaceView
+    new_place: PlaceView
+
+    def __call__(self):
+        if self.current_place is not None and self.current_place.last_update > self.new_place.last_update:
+            raise OlderPlaceWhenUpdateRuleError(
+                f"Place({self.current_place.id}); current_update: {self.new_place.last_update}, new_update: {self.current_place.last_update}"
+            )
