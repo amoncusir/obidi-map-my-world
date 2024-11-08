1178780ba1245bad8b8e465c93a43fe3777ec86a
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 01:41:54 +0100

    add debug message

--- src/module/common/infrastructure/faststream/integration_events_bus.py
+++ src/module/common/infrastructure/faststream/integration_events_bus.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from logging import getLogger
 
 from faststream.rabbit import RabbitBroker, RabbitExchange
 
@@ -8,6 +9,8 @@ from src.module.common.application.event.integration_bus import (
     IntegrationEventTask,
 )
 
+logger = getLogger(__name__)
+
 
 @dataclass
 class FastStreamIntegrationEventsBus(IntegrationEventsBus):
@@ -16,5 +19,9 @@ class FastStreamIntegrationEventsBus(IntegrationEventsBus):
     exchange: RabbitExchange
 
     async def async_publish(self, event: IntegrationEvent) -> IntegrationEventTask:
+
         await self.broker.publish(event, routing_key=event.name(), message_id=str(event.id), exchange=self.exchange)
+
+        logger.debug("Event Published: %s", repr(event))
+
         return IntegrationEventTask(event.id)
