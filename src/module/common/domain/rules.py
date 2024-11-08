73f3d59f49c56c672e95cb746bb4d5c687c82299
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 14:44:45 +0100

    add classmethods to trigger rules

--- src/module/common/domain/rules.py
+++ src/module/common/domain/rules.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 from dataclasses import dataclass
+from typing import NoReturn, Optional
 
 from src.module.common.domain.errors import DomainError
 
@@ -11,8 +12,19 @@ class RuleError(DomainError):
 @dataclass(frozen=True, kw_only=True)
 class Rule:
 
-    def __post_init__(self):
-        self()
-
     @abstractmethod
     def __call__(self): ...
+
+    @classmethod
+    def spark(cls, **kwargs) -> Optional[NoReturn]:
+        rule = cls(**kwargs)
+        rule()
+
+    @classmethod
+    def check(cls, **kwargs) -> bool:
+        rule = cls(**kwargs)
+        try:
+            rule()
+            return True
+        except RuleError:
+            return False
