5fd06ee6b4b001f1f8945ecc98817edee7c4ef94
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 17:47:27 +0100

    added a repository contract

---
+++ src/module/recommendation/domain/recommendation/repository.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+
+from src.module.recommendation.domain.recommendation.projections import PlaceID
+from src.module.recommendation.domain.recommendation.recommendation import (
+    Recommendation,
+)
+
+
+class RecommendationRepository:
+
+    @abstractmethod
+    async def create_recommendation(self, recommendation: Recommendation): ...
+
+    @abstractmethod
+    async def update_score(self, recommendation: Recommendation): ...
+
+    @abstractmethod
+    async def update_place_view(self, recommendation: Recommendation): ...
+
+    @abstractmethod
+    async def find_recommendation_by_place_id(self, recommendation_id: PlaceID) -> Recommendation: ...
