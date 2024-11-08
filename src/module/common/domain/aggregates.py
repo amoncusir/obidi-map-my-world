5b4effa2a28f5307982ad9ec22de864f7295d235
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 15:24:35 +0100

    added a log error when aggregate is cleaned without parsing the remaining events

--- src/module/common/domain/aggregates.py
+++ src/module/common/domain/aggregates.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from typing import List
 
 from pydantic import PrivateAttr
@@ -5,10 +6,17 @@ from pydantic import PrivateAttr
 from src.module.common.domain.entities import DomainEntity
 from src.module.common.domain.events import DomainEvent
 
+logger = getLogger(__name__)
+
 
 class AggregateRoot[ID](DomainEntity[ID]):
 
     __events_queue: List[DomainEvent] = PrivateAttr(default_factory=list)
+
+    def __del__(self):
+        if len(self.__events_queue) > 0:
+            class_name = self.__class__.__qualname__
+            logger.error(f"Remaining events on {class_name} when destructor called! :: %s", self.__events_queue)
 
     def _add_event(self, event: DomainEvent):
         self.__events_queue.append(event)
