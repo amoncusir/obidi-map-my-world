87eb11dcb19c78e40c91c5a0a60aef9f8a8772ae
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 12:43:29 +0100

    rename module

--- src/module/recommendation/application/http/router.py
+++ src/module/recommendation/application/http/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 # Module routers
-from .place import router as place_router
+from .recommend import router as place_router
 
 router = APIRouter()
 router.include_router(place_router)
