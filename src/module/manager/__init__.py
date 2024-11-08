de4973d4d2074ed86b387aea984a2cb8d87c6506
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 15:13:06 +0100

    set __all__ variable to force the intellisense import scope

--- src/module/manager/__init__.py
+++ src/module/manager/__init__.py
@@ -5,3 +5,12 @@ from .application.integration_event.events import (
 )
 from .domain.category.projections import CategoryProjection
 from .domain.place.projections import PlaceProjection, ReviewProjection
+
+__all__ = [
+    CreatedPlaceApplicationEvent,
+    ReviewAddedApplicationEvent,
+    UpdatedPlaceApplicationEvent,
+    CategoryProjection,
+    PlaceProjection,
+    ReviewProjection,
+]
