6405d068ebcff697e0771fc8f3f5f871eb862dd3
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 12:19:40 +0100

    added ED entry points

--- src/module/recommendation/application/integration_event/handlers.py
+++ src/module/recommendation/application/integration_event/handlers.py
@@ -1,7 +1,25 @@
 from src.module.common.application.event.integration import IntegrationEventSubscriber
 from src.module.manager.application.integration_event.events import (
+    CreatedPlaceApplicationEvent,
     ReviewAddedApplicationEvent,
 )
+
+
+class CreatedPlaceIntegrationEventSubscriber(IntegrationEventSubscriber[CreatedPlaceApplicationEvent]):
+    """
+    Subscription to CreatedPlaceApplicationEvent.
+    Create a new recommendation
+    """
+
+    async def handle_event(self, event: CreatedPlaceApplicationEvent): ...
+
+    @classmethod
+    def name(cls) -> str:
+        return "place.created.to.recommendation"
+
+    @classmethod
+    def routing_key(cls) -> str:
+        return "place.created"
 
 
 class ReviewAddedIntegrationEventSubscriber(IntegrationEventSubscriber[ReviewAddedApplicationEvent]):
@@ -10,9 +28,7 @@ class ReviewAddedIntegrationEventSubscriber(IntegrationEventSubscriber[ReviewAdd
     Pass through the recommendation layer to decide if it changes
     """
 
-    async def handle_event(self, event: ReviewAddedApplicationEvent):
-        print(event)
-        print("AMZNG!")
+    async def handle_event(self, event: ReviewAddedApplicationEvent): ...
 
     @classmethod
     def name(cls) -> str:
