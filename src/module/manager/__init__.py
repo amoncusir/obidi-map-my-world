46c3d0761fa9c58502e02a562a2702bb4367e68e
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 12:43:19 +0100

    expose the public objects

---
+++ src/module/manager/__init__.py
@@ -0,0 +1,7 @@
+from .application.integration_event.events import (
+    CreatedPlaceApplicationEvent,
+    ReviewAddedApplicationEvent,
+    UpdatedPlaceApplicationEvent,
+)
+from .domain.category.projections import CategoryProjection
+from .domain.place.projections import PlaceProjection, ReviewProjection
