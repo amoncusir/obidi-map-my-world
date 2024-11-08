17052e6b39ee099febc6f426dda52c7b1c1eebf1
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-11-08 17:44:32 +0100

    implemented subscribers

--- src/module/recommendation/application/integration_event/handlers.py
+++ src/module/recommendation/application/integration_event/handlers.py
@@ -1,14 +1,41 @@
+from dataclasses import dataclass
+
+from src.module.common.application.event.domain_bus import DomainEventBus
 from src.module.common.application.event.integration import IntegrationEventSubscriber
 from src.module.manager import CreatedPlaceApplicationEvent, ReviewAddedApplicationEvent
+from src.module.recommendation.domain.recommendation.projections import PlaceView
+from src.module.recommendation.domain.recommendation.recommendation import (
+    Recommendation,
+)
+from src.module.recommendation.domain.recommendation.repository import (
+    RecommendationRepository,
+)
 
 
+@dataclass
 class CreatedPlaceIntegrationEventSubscriber(IntegrationEventSubscriber[CreatedPlaceApplicationEvent]):
     """
     Subscription to CreatedPlaceApplicationEvent.
     Create a new recommendation
     """
 
-    async def handle_event(self, event: CreatedPlaceApplicationEvent): ...
+    domain_event_bus: DomainEventBus
+    recommendation_repository: RecommendationRepository
+
+    async def handle_event(self, event: CreatedPlaceApplicationEvent):
+
+        place_view = PlaceView(
+            id=event.place_projection.id,
+            name=event.place_projection.name,
+            last_update=event.place_projection.projected_at,
+            location=event.place_projection.location,
+            total_reviews=len(event.place_projection.reviews),
+        )
+
+        recommendation = Recommendation.create(place=place_view)
+
+        await self.recommendation_repository.create_recommendation(recommendation)
+        await self.domain_event_bus.async_process_aggregate(recommendation)
 
     @classmethod
     def name(cls) -> str:
@@ -25,7 +52,25 @@ class ReviewAddedIntegrationEventSubscriber(IntegrationEventSubscriber[ReviewAdd
     Pass through the recommendation layer to decide if it changes
     """
 
-    async def handle_event(self, event: ReviewAddedApplicationEvent): ...
+    domain_event_bus: DomainEventBus
+    recommendation_repository: RecommendationRepository
+
+    async def handle_event(self, event: ReviewAddedApplicationEvent):
+
+        recommendation = await self.recommendation_repository.find_recommendation_by_place_id(event.place_projection.id)
+
+        place_view = PlaceView(
+            id=event.place_projection.id,
+            name=event.place_projection.name,
+            last_update=event.place_projection.projected_at,
+            location=event.place_projection.location,
+            total_reviews=len(event.place_projection.reviews),
+        )
+
+        recommendation.update_place(place_view)
+
+        await self.recommendation_repository.update_place_view(recommendation)
+        await self.domain_event_bus.async_process_aggregate(recommendation)
 
     @classmethod
     def name(cls) -> str:
